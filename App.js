36d54e0f91cb079f2bcadbeccdf0456de92d76c7
Park Youngsun <55642520+Park-youngsun@users.noreply.github.com>
2022-03-09 21:26:09 +0900

    Add files via upload
    components

---
+++ App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import Counter from './components/Counter'
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <h1> Youngsun Love</h1>
+      <Counter />
+      <Counter />
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
